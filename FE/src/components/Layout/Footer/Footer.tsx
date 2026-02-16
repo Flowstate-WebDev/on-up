3e7b495c128a23f49deb770d61a98ea345677b6f
33jarek <33patryk.jarosz@gmail.com>
2026-02-16 22:36:28 +0100

    grayscale logo

--- FE/src/components/Layout/Footer/Footer.tsx
+++ FE/src/components/Layout/Footer/Footer.tsx
@@ -1,33 +1,40 @@
-import { Link } from "@tanstack/react-router"
+import { Link } from "@tanstack/react-router";
 
-import { Button } from "@/components/ui/Button"
+import { Button } from "@/components/ui/Button";
+import Logo from "../Header/Logo";
 
 export const Footer = () => {
   return (
-    <div className='flex flex-col md:flex-row w-full xl:w-2/3 xl:mx-auto items-stretch justify-center px-8 bg-primary text-text-obj p-6 rounded-t-lg divider-lines'>
-      <div className='flex grow gap-1 mb-6 md:mb-0 justify-center items-center'>
-        <img src={'/images/onup_logo.webp'} alt={'logo'} height={52} width={52} className='grayscale' />
-        <div className='flex flex-col'>
-          <p className="text-1xl font-semibold text-white leading-none">Wydawnictwo</p>
-          <h1 className="text-2xl font-bold text-white leading-none">On-Up</h1>
-        </div>
+    <div className="flex flex-col md:flex-row w-full xl:w-2/3 xl:mx-auto items-stretch justify-center px-8 bg-primary text-text-obj p-6 rounded-t-lg divider-lines">
+      <div className="flex grow gap-1 mb-6 md:mb-0 justify-center items-center">
+        <Logo grayscale />
       </div>
       <div className="flex grow flex-col text-center justify-center items-center">
         <div className="flex gap-2">
-          <img className="w-4 h-4 brightness-0 invert self-center" src="/icons/phone-icon.svg" alt="" />
+          <img
+            className="w-4 h-4 brightness-0 invert self-center"
+            src="/icons/phone-icon.svg"
+            alt=""
+          />
           <p>506 610 405</p>
         </div>
         <div className="flex gap-2 mb-4">
-          <img className="w-4 h-4 brightness-0 invert self-center" src="/icons/mail-icon.svg" alt="" />
+          <img
+            className="w-4 h-4 brightness-0 invert self-center"
+            src="/icons/mail-icon.svg"
+            alt=""
+          />
           <p>onup.wydawnictwo@gmail.com</p>
         </div>
-        <Button style={"outline_white"} type={"button"}><Link to={'/polityka'}>Polityka strony</Link></Button>
+        <Button style={"outline_white"} type={"button"}>
+          <Link to={"/polityka"}>Polityka strony</Link>
+        </Button>
       </div>
-      <div className='grow mt-6 md:mt-0 text-center flex flex-col justify-center items-center'>
+      <div className="grow mt-6 md:mt-0 text-center flex flex-col justify-center items-center">
         <p>ul. Gajowa 14/2, 82-500 Rakowiec</p>
         <p>NIP 5811228975, REGON 192050931</p>
         <p>Firma wpisana do CEIDG</p>
       </div>
     </div>
-  )
-}
+  );
+};
--- FE/src/components/Layout/Header/Logo.tsx
+++ FE/src/components/Layout/Header/Logo.tsx
@@ -1,13 +1,31 @@
-import { Link } from '@tanstack/react-router';
+import { Link } from "@tanstack/react-router";
 
-export default function Logo() {
+interface LogoProps {
+  grayscale?: boolean;
+}
+
+export default function Logo({ grayscale }: LogoProps) {
   return (
     <div className="w-fit">
-      <Link to={'/'} id='logo' className='flex gap-1 items-center'>
-        <img src="/images/onup_logo.webp" alt="Logo wydawnictwa" width={64} height={64} className="h-16 w-auto" />
+      <Link
+        to={"/"}
+        id="logo"
+        className={`flex gap-1 items-center ${grayscale ? "grayscale opacity-80" : ""}`}
+      >
+        <img
+          src="/images/onup_logo.webp"
+          alt="Logo wydawnictwa"
+          width={64}
+          height={64}
+          className="h-16 w-auto"
+        />
         <div>
-          <p className="text-1xl font-semibold text-text-secondary leading-none">Wydawnictwo</p>
-          <h1 className="text-2xl font-bold text-text-primary leading-none">On-Up</h1>
+          <p className="text-1xl font-semibold text-text-secondary leading-none">
+            Wydawnictwo
+          </p>
+          <h1 className="text-2xl font-bold text-text-primary leading-none">
+            On-Up
+          </h1>
         </div>
       </Link>
     </div>

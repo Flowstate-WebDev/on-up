94a05e6e5047dcacb7bdeacb933a33a170d6d16e
Pinkify_ <huelleigor@gmail.com>
2026-02-16 23:17:16 +0100

    Logo export fix

--- FE/src/components/Layout/Header/Logo.tsx
+++ FE/src/components/Layout/Header/Logo.tsx
@@ -1,15 +1,16 @@
 import { Link } from "@tanstack/react-router";
+import { getAssetPath } from "@/utils/paths";
 
 interface LogoProps {
   grayscale?: boolean;
 }
 
-export default function Logo({ grayscale }: LogoProps) {
+export const Logo = ({ grayscale }: LogoProps) => {
   return (
     <div className="w-fit">
       <Link to={"/"} id="logo" className={`flex gap-1 items-center`}>
         <img
-          src="/images/onup_logo.webp"
+          src={getAssetPath("/images/onup_logo.webp")}
           alt="Logo wydawnictwa"
           width={64}
           height={64}

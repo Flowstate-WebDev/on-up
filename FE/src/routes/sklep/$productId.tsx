dd5556260d7784d71ba033b27907c35b9b54037b
33jarek <33patryk.jarosz@gmail.com>
2026-03-12 14:40:02 +0100

    desc size, deleting book fix ig

--- FE/src/routes/sklep/$productId.tsx
+++ FE/src/routes/sklep/$productId.tsx
@@ -1,35 +1,35 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-import { useProduct } from '@/hooks/queries/product/useProduct'
+import { useProduct } from "@/hooks/queries/product/useProduct";
 
-import { ProductImageGalery } from '@/routes/sklep/components/ProductImageGalery'
-import { ProductDetailsSection } from '@/routes/sklep/components/ProductDetailsSection'
-import { ProductDescriptionSection } from '@/routes/sklep/components/ProductDescriptionSection'
-import { QueryState } from '@/components/ui/QueryState'
+import { ProductImageGalery } from "@/routes/sklep/components/ProductImageGalery";
+import { ProductDetailsSection } from "@/routes/sklep/components/ProductDetailsSection";
+import { ProductDescriptionSection } from "@/routes/sklep/components/ProductDescriptionSection";
+import { QueryState } from "@/components/ui/QueryState";
 
-export const Route = createFileRoute('/sklep/$productId')({
+export const Route = createFileRoute("/sklep/$productId")({
   component: ProductPage,
-})
+});
 
 function ProductPage() {
-  const { productId } = Route.useParams()
-  const { isPending, error, data: product } = useProduct(productId)
+  const { productId } = Route.useParams();
+  const { isPending, error, data: product } = useProduct(productId);
 
   if (isPending) {
-    return <QueryState>Ładowanie produktu...</QueryState>
+    return <QueryState>Ładowanie produktu...</QueryState>;
   }
 
   if (error || !product) {
-    return <QueryState>Ups... nie udało się pobrać produktu.</QueryState>
+    return <QueryState>Ups... nie udało się pobrać produktu.</QueryState>;
   }
 
   return (
     <div>
-      <div className='flex flex-col items-center gap-y-10 md:flex-row md:justify-center-safe md:gap-x-42'>
+      <div className="flex flex-col items-center gap-y-10 md:flex-row md:justify-center-safe md:gap-x-42">
         <ProductImageGalery imageUrl={product.imageUrl} />
         <ProductDetailsSection product={product} />
       </div>
       <ProductDescriptionSection description={product.description} />
     </div>
-  )
+  );
 }
--- FE/src/routes/sklep/components/ProductDescriptionSection/ProductDescriptionSection.tsx
+++ FE/src/routes/sklep/components/ProductDescriptionSection/ProductDescriptionSection.tsx
@@ -1,12 +1,12 @@
 type Props = {
-  description: string
-}
+  description: string;
+};
 
 export function ProductDescriptionSection({ description }: Props) {
   return (
-    <div className="py-16">
-      <h1 className='text-2xl font-bold mb-4'>Opis</h1>
+    <div className="py-16 mx-auto w-full text-center max-w-2/4">
+      <h1 className="text-2xl font-bold mb-4">Opis</h1>
       <p>{description}</p>
     </div>
-  )
+  );
 }

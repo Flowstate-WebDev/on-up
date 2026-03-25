ec44941005b5ba6aa23af36424d6efbe97de7145
Pinkify_ <huelleigor@gmail.com>
2026-03-25 13:40:13 +0100

    ą

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,8 +1,10 @@
 import app from "./server.js";
 import { startOrderCleanupJob } from "./lib/cleanup.js";
 
-app.listen(3001, () => {
-  console.log("\x1b[92m%s\x1b[0m", "Server running on PORT 3001!");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("\x1b[92m%s\x1b[0m", `Server running on PORT ${PORT}!`);
 
   // Start periodic cleanup of expired pending orders
   // Interval: 2 minutes
